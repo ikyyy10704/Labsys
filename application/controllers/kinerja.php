41ccabe0752ed5b86357fe7ca5da398e927f394c
Lingard-cyber <rayanborn95@gmail.com>
2025-01-22 23:06:29 +0700

    commit

--- application/controllers/kinerja.php
+++ application/controllers/kinerja.php
@@ -12,51 +12,6 @@ class Kinerja extends CI_Controller {
 
     public function index() {
         $data['title'] = 'Data Kinerja Karyawan';
-<<<<<<< HEAD
-        $data['kinerja'] = $this->Kinerja_model->get_kinerja_karyawan();
-        
-        $this->load->view('template/header', $data);
-        $this->load->view('template/sidebar');
-        $this->load->view('template/footer');
-        $this->load->view('kinerja/kinerja_list', $data);
-    }
-
-    
-        public function tambah() {
-            $data['title'] = 'Tambah Data Kinerja';
-            $data['karyawan'] = $this->Kinerja_model->get_active_karyawan();
-            $data['manajer'] = $this->Kinerja_model->get_all_manajer();
-        
-            $this->form_validation->set_rules('id_krywn', 'Karyawan', 'required');
-            $this->form_validation->set_rules('nilai_kerja', 'Nilai Kerja', 'required|numeric|greater_than_equal_to[0]|less_than_equal_to[100]');
-            $this->form_validation->set_rules('status_pengelolaan', 'Status Pengelolaan', 'required');
-            $this->form_validation->set_rules('id_manajer', 'Manajer', 'required');
-            $this->form_validation->set_rules('tgl_pengelolaan', 'Tanggal Pengelolaan', 'required');
-        
-            if ($this->form_validation->run() === FALSE) {
-                $this->load->view('template/header', $data);
-                $this->load->view('template/sidebar');
-                $this->load->view('template/footer');
-                $this->load->view('kinerja/kinerja_tambah', $data);
-            } else {
-                $kinerja_data = [
-                    'nilai_kerja' => $this->input->post('nilai_kerja'),
-                    'nilai_kerja' => $this->input->post('nilai_kerja'),
-                    'status_pengelolaan' => $this->input->post('status_pengelolaan'),
-                    'tgl_pengelolaan' => $this->input->post('tgl_pengelolaan'),
-                    'id_manajer' => $this->input->post('id_manajer')
-                ];
-                $id_krywn = $this->input->post('id_krywn');
-        
-                if ($this->Kinerja_model->insert_kinerja($kinerja_data, $id_krywn)) {
-                    $this->session->set_flashdata('success', 'Data kinerja berhasil ditambahkan');
-                } else {
-                    $this->session->set_flashdata('error', 'Gagal menambahkan data kinerja');
-                }
-                redirect('kinerja');
-            }
-        }
-=======
         $data['kinerja'] = $this->Kinerja_model->get_kinerja_karyawan(); 
         $this->load->view('template/header', $data);
         $this->load->view('template/sidebar');
@@ -70,7 +25,6 @@ class Kinerja extends CI_Controller {
         $data['manajer1'] = $this->Kinerja_model->get_managers();
         $data['manajer'] = $this->Kinerja_model->get_all_manajer();
         
->>>>>>> 45f8a1b3af6ae2880fabb8fee4b4c65009d7926e
 
         $this->form_validation->set_rules('status_pengelolaan', 'Status Pengelolaan', 'required');
         $this->form_validation->set_rules('tgl_pengelolaan', 'Tanggal Pengelolaan', 'required');
--- application/models/Kinerja_model.php
+++ application/models/Kinerja_model.php
@@ -23,98 +23,6 @@ class Kinerja_model extends CI_Model {
     }
 
     public function get_kinerja_by_id($id) {
-<<<<<<< HEAD
-        $this->db->select('kk.id_pengelolaan, kk.nilai_kerja, kk.status_pengelolaan, 
-                      kk.tgl_pengelolaan, kk.id_manajer, dk.nama_krywn, 
-                      m.nama_manajer, m.departemen');
-    $this->db->from('kinerja_karyawan kk');
-    $this->db->join('data_karyawan dk', 'kk.id_pengelolaan = dk.id_pengelolaan');
-    $this->db->join('manajer m', 'kk.id_manajer = m.id_manajer');
-    $this->db->where('kk.id_pengelolaan', $id);
-    return $this->db->get()->row();
-    }
-    public function get_kinerja_by_date($date) {
-        $this->db->select('kk.*, dk.nama_krywn, m.nama_manajer, m.departemen');
-        $this->db->from('kinerja_karyawan kk');
-        $this->db->join('data_karyawan dk', 'kk.id_pengelolaan = dk.id_pengelolaan');
-        $this->db->join('manajer m', 'kk.id_manajer = m.id_manajer');
-        $this->db->where('DATE(kk.tgl_pengelolaan)', $date);
-        return $this->db->get()->result();
-    }
-
-    public function insert_kinerja($data, $id_krywn) {
-        $this->db->trans_start();
-        try {
-            $this->db->insert('kinerja_karyawan', $data);
-            $id_pengelolaan = $this->db->insert_id();
-            $this->db->where('id_krywn', $id_krywn);
-            $this->db->update('data_karyawan', ['id_pengelolaan' => $id_pengelolaan]);
-            $this->db->where('id_krywn', $id_krywn);
-            $this->db->order_by('tanggal', 'DESC');
-            $this->db->limit(1);
-            $absensi = $this->db->get('absensi');
-            
-            if ($absensi->num_rows() > 0) {
-                $this->db->where('id_pengelolaan', $id_pengelolaan);
-                $this->db->update('kinerja_karyawan', [
-                    'id_absensi' => $absensi->row()->id_absensi
-                ]);
-            }
-    
-            $this->db->trans_complete();
-            return $this->db->trans_status();
-    
-        } catch (Exception $e) {
-            $this->db->trans_rollback();
-            log_message('error', 'Error inserting kinerja: ' . $e->getMessage());
-            return false;
-        }
-    }
-    public function update_kinerja($id_pengelolaan, $data) {
-        $this->db->trans_start();
-        try {
-            // Validasi nilai kerja
-            if (!is_numeric($data['nilai_kerja']) || $data['nilai_kerja'] < 0 || $data['nilai_kerja'] > 100) {
-                return false;
-            }
-    
-            // Update data kinerja
-            $this->db->where('id_pengelolaan', $id_pengelolaan);
-            $result = $this->db->update('kinerja_karyawan', [
-                'nilai_kerja' => $data['nilai_kerja'],
-                'status_pengelolaan' => $data['status_pengelolaan'],
-                'tgl_pengelolaan' => $data['tgl_pengelolaan'],
-                'id_manajer' => $data['id_manajer']
-            ]);
-    
-            if (!$result) {
-                throw new Exception('Gagal update data kinerja');
-            }
-    
-            $this->db->trans_complete();
-            return $this->db->trans_status();
-    
-        } catch (Exception $e) {
-            $this->db->trans_rollback();
-            log_message('error', 'Error updating kinerja: ' . $e->getMessage());
-            return false;
-        }
-    }
-    
-    
-    public function get_all_manajer() {
-        return $this->db->get('manajer')->result();
-    }
-
-    public function get_active_karyawan() {
-        $this->db->select('id_krywn, nama_krywn, posisi');
-        $this->db->from('data_karyawan');
-        $this->db->where('status', 'Aktif');
-        $this->db->order_by('nama_krywn', 'ASC');
-        return $this->db->get()->result();
-    }
-
-=======
         $this->db->where('id_pengelolaan', $id);
         $query = $this->db->get('vw_kinerja_detail');
         return $query->row();
@@ -186,7 +94,6 @@ class Kinerja_model extends CI_Model {
         return $this->db->get()->result();
     }
 
->>>>>>> 45f8a1b3af6ae2880fabb8fee4b4c65009d7926e
     public function get_kinerja_grade($nilai) {
         $this->db->select("fn_grade_kinerja($nilai) as grade");
         return $this->db->get()->row()->grade;
@@ -196,30 +103,4 @@ class Kinerja_model extends CI_Model {
         $this->db->select("fn_hitung_bonus_tahunan($masa_kerja, $nilai_kinerja) as bonus");
         return $this->db->get()->row()->bonus;
     }
-<<<<<<< HEAD
-    public function delete_kinerja($id) {
-        $this->db->trans_start();
-        try {
-            // Hapus data gaji yang terkait terlebih dahulu
-            $this->db->where('id_pengelolaan', $id);
-            $this->db->delete('gaji_karyawan');
-            
-            // Update id_pengelolaan di data_karyawan menjadi null
-            $this->db->where('id_pengelolaan', $id);
-            $this->db->update('data_karyawan', ['id_pengelolaan' => null]);
-            
-            // Hapus data kinerja
-            $this->db->where('id_pengelolaan', $id);
-            $this->db->delete('kinerja_karyawan');
-            
-            $this->db->trans_complete();
-            return $this->db->trans_status();
-        } catch (Exception $e) {
-            $this->db->trans_rollback();
-            log_message('error', 'Error deleting kinerja: ' . $e->getMessage());
-            return false;
-        }
-    }
-=======
->>>>>>> 45f8a1b3af6ae2880fabb8fee4b4c65009d7926e
 }
