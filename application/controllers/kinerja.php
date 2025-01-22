c80c2905c49b0d70c17057d9d10cff9e5a1d309e
ikyyy10704 <riskypangestu057@gmail.com>
2025-01-22 23:04:13 +0700

    KINERJA BARU

--- application/controllers/kinerja.php
+++ application/controllers/kinerja.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Kinerja extends CI_Controller {
-    
+
     public function __construct() {
         parent::__construct();
         $this->load->model('Kinerja_model');
@@ -12,7 +12,6 @@ class Kinerja extends CI_Controller {
 
     public function index() {
         $data['title'] = 'Data Kinerja Karyawan';
-<<<<<<< HEAD
         $data['kinerja'] = $this->Kinerja_model->get_kinerja_karyawan();
         
         $this->load->view('template/header', $data);
@@ -56,63 +55,59 @@ class Kinerja extends CI_Controller {
                 redirect('kinerja');
             }
         }
-=======
-        $data['kinerja'] = $this->Kinerja_model->get_kinerja_karyawan(); 
-        $this->load->view('template/header', $data);
-        $this->load->view('template/sidebar');
-        $this->load->view('kinerja/kinerja_list.php', $data);
-        $this->load->view('template/footer');
-    }
 
-    public function tambah() {
-        $data['title'] = 'Tambah Data Kinerja';
-        $data['karyawan'] = $this->Kinerja_model->get_active_karyawan();
-        $data['manajer1'] = $this->Kinerja_model->get_managers();
-        $data['manajer'] = $this->Kinerja_model->get_all_manajer();
-        
->>>>>>> 45f8a1b3af6ae2880fabb8fee4b4c65009d7926e
-
-        $this->form_validation->set_rules('status_pengelolaan', 'Status Pengelolaan', 'required');
-        $this->form_validation->set_rules('tgl_pengelolaan', 'Tanggal Pengelolaan', 'required');
-        
-        $data = array(
-            'id_krywn' => $this->input->post('id_krywn'),
-            'id_absensi' => $this->input->post('id_absensi'),
-            'tgl_pengelolaan' => $this->input->post('tgl_pengelolaan'),
-            'nilai_kerja' => $this->input->post('nilai_kerja'),
-            'status_pengelolaan' => $this->input->post('status_pengelolaan'),
-            'id_manajer' => $this->input->post('id_manajer')
-        );
-        
-        $this->load->view('template/header', $data);
-        $this->load->view('template/sidebar');
-        $this->load->view('template/footer');
-        $this->load->view('kinerja/kinerja_tambah', $data);
+    public function get_departemen_by_manajer() {
+        $id_manajer = $this->input->post('id_manajer');
+        $departemen = $this->Kinerja_model->get_departemen_by_manajer($id_manajer);
+        header('Content-Type: application/json');
+        echo json_encode(['departemen' => $departemen ? $departemen : '']);
     }
     
-
-    public function edit($id) {
-     $this->form_validation->set_rules('status_pengelolaan', 'Status Pengelolaan', 'required');
-    $this->form_validation->set_rules('tgl_pengelolaan', 'Tanggal Pengelolaan', 'required');
-    $data = array(
-        'nilai_kerja' => $this->input->post('nilai_kerja'),
-        'status_pengelolaan' => $this->input->post('status_pengelolaan'),
-        'tgl_pengelolaan' => $this->input->post('tgl_pengelolaan'),
-        'id_manajer' => $this->input->post('id_manajer')
-    );
+    public function edit($id = null) {
+        $selected_date = $this->input->get('date');
+    
+        if (!$id) {
+            $this->session->set_flashdata('error', 'ID Pengelolaan tidak ditemukan');
+            redirect('kinerja');
+        }
         $data['title'] = 'Edit Data Kinerja';
         $data['kinerja'] = $this->Kinerja_model->get_kinerja_by_id($id);
         $data['manajer'] = $this->Kinerja_model->get_all_manajer();
-        
+        if ($selected_date) {
+            $data['kinerja_by_date'] = $this->Kinerja_model->get_kinerja_by_date($selected_date);
+        }
+    
         if (!$data['kinerja']) {
             $this->session->set_flashdata('error', 'Data kinerja tidak ditemukan');
             redirect('kinerja');
         }
-        
-        $this->load->view('template/header', $data);
-        $this->load->view('template/sidebar');
-        $this->load->view('kinerja/kinerja_edit', $data);
-        $this->load->view('template/footer');
+    
+        $this->form_validation->set_rules('nilai_kerja', 'Nilai Kerja', 'required|numeric');
+        $this->form_validation->set_rules('status_pengelolaan', 'Status Pengelolaan', 'required');
+        $this->form_validation->set_rules('id_manajer', 'Manajer', 'required');
+        $this->form_validation->set_rules('tgl_pengelolaan', 'Tanggal Pengelolaan', 'required');
+    
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('template/header', $data);
+            $this->load->view('template/sidebar');  
+            $this->load->view('template/footer');
+            $this->load->view('kinerja/kinerja_edit', $data);
+        } else {
+            $update_data = [
+                'nilai_kerja' => $this->input->post('nilai_kerja'),
+                'status_pengelolaan' => $this->input->post('status_pengelolaan'),
+                'tgl_pengelolaan' => $this->input->post('tgl_pengelolaan'),
+                'id_manajer' => $this->input->post('id_manajer')
+            ];
+    
+            if ($this->Kinerja_model->update_kinerja($id, $update_data)) {
+                $this->session->set_flashdata('success', 'Data kinerja berhasil diperbarui');
+                redirect('kinerja');
+            } else {
+                $this->session->set_flashdata('error', 'Gagal memperbarui data kinerja');
+                redirect('kinerja/edit/' . $id);
+            }
+        }
     }
     public function hapus($id) {
         if ($this->Kinerja_model->delete_kinerja($id)) {
